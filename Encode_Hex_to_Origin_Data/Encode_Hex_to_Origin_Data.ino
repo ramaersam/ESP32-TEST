dac51487a26beccb8e25bbfcd21c0d42b0c25134
Ahmad Ersam Nur Ramadhan <ramaersam@gmail.com>
2023-09-20 15:23:51 +0700

    Update Encode_Hex_to_Origin_Data.ino
    fungtion for decode hex ready to send json, note for dimmer is not yet ready

--- Encode_Hex_to_Origin_Data/Encode_Hex_to_Origin_Data.ino
+++ Encode_Hex_to_Origin_Data/Encode_Hex_to_Origin_Data.ino
@@ -21,6 +21,7 @@
                                                    12 11 10 9  8  7  --> bit
 //09:17:58.447 -> Zigbee Data ke [5]:[ 0 0 0 0 0 5 FF FF FF FF FF FF 1 0 0 0 0 ]
                                                    6  5  4  3  2  1 --> bit
+**********************************************************************************************/                                                   
 
 /************************CMD 3000 Kinetic ******************************************************
 //09:53:14.084 -> Received data: {"feedback":"Kinetic","CMD":3000,"status":"0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,","OK":1}
@@ -35,15 +36,25 @@ String dimstatusData[7];
 int kinstatusData[6];
 String binaryValue[6];
 String data;
+String binaryValues[6];
+// Initialize a string to store the combined binary values
+String combinedBinary = "";
+int indexNum = 0;
+String addressData = "";
+String statusU8[6];
+//String combinedU8 = "";
+// Initialize combinedU8 as a string with all zeros
+String combinedU8 = "{0,0,0,0,0,0}","{0,0,0,0,0,0}","{0,0,0,0,0,0}","{0,0,0,0,0,0}","{0,0,0,0,0,0}","{0,0,0,0,0,0}";
 
 void setup() {
   Serial.begin(115200);
 }
 
 void encodeKinetic(String data) {
+
   // Memisahkan data berdasarkan spasi
   String extractData[data.length()]; // Sesuaikan ukuran array sesuai dengan kebutuhan
-  int indexNum = 0;
+
   while (data.length() > 0) {
     int spaceIndex = data.indexOf(' ');
     if (spaceIndex >= 0) {
@@ -76,13 +87,40 @@ void encodeKinetic(String data) {
 
   Serial.print("statusData in BIN: ");
   for (int i = 0; i < 6; i++) {
-    Serial.print(binaryValue[i]);
-    Serial.print(",");
+    String binary = binaryValue[i];
+    
+   // Pad each binary value with leading zeros to ensure it has a length of 8
+    while (binary.length() < 6) {
+      binary = "0" + binary ;
+    }
+    
+    // Assign the binary value to the binaryValues array
+    binaryValues[i] = binary;
+    Serial.print(binaryValues[i]);
+    Serial.print(" ");
+
+    // Add a comma after each bit in the binary representation
+    for (int j = 0; j < binary.length(); j++) {
+      combinedBinary += binary[j];
+      if (j < binary.length() - 1) {
+        combinedBinary += ",";
+      }
+    }
+
+    // Add a space between binary values if it's not the last binary value
+    if (i < 5) {
+      combinedBinary += ",";
+    }
   }
   Serial.println();
+  Serial.print("combinedBinary: ");
+  // Print the combined binary values without any extra characters
+  Serial.println(combinedBinary);
+
 }
 
 void encodeDimmer(String data){
+  
   // Memisahkan data berdasarkan spasi
   String extractData[data.length()]; // Sesuaikan ukuran array sesuai dengan kebutuhan
   int indexNum = 0;
@@ -109,14 +147,25 @@ void encodeDimmer(String data){
   // Ambil indeks 6 hingga 11 dari extractData, masukkan ke variabel statusData
   Serial.print("dimstatusData: ");
   for (int i = 5; i < 12; i++) {
-      dimstatusData[i - 6] = strtol(extractData[i].c_str(), NULL, HEX); //Konversi Hex ke Dec
-      Serial.print(dimstatusData[i - 6]);
+      dimstatusData[i - 5] = strtol(extractData[i].c_str(), NULL, HEX); //Konversi Hex ke Dec
+      Serial.print(dimstatusData[i - 5]);
       Serial.print(" ");
   }
   Serial.println();
 
-  //store to matrix here
-    
+  Serial.print("addressData: ");
+  addressData = String(dimstatusData[0]); // Ubah menjadi String
+  Serial.print(addressData);
+  Serial.println();
+
+  Serial.print("statusU8: ");
+  for (int i = 1; i < 6; i++) {
+    statusU8[i - 1] = String(dimstatusData[i]+ ","); // Ubah dan sesuaikan indeksnya
+    Serial.print(statusU8[i - 1]);
+  }
+
+  //put code to combineU8 here
+
 }
 
 void loop() {
@@ -124,7 +173,7 @@ void loop() {
     // Baca data dari port serial
     data = Serial.readStringUntil('\n');
     // Panggil fungsi encode untuk memproses data
-    encodeKinetic(data);
-    //encodeDimmer(data);
+    //encodeKinetic(data);
+    encodeDimmer(data);
   }
 }
